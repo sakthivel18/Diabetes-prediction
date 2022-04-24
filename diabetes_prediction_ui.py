ed10f87bf09d46c10f11b237eab5a15e8b9e6d1b
Sakthivel Ravichandran <sakthivelravichandran18@gmail.com>
2022-04-24 09:58:17 -0400

    changed knn model to random model

--- diabetes_prediction_ui.py
+++ diabetes_prediction_ui.py
@@ -23,7 +23,7 @@ This app predicts Diabetes using KNN model!
 st.sidebar.write("""
 ### User Input Features
 """)
-knn_model = load_random_model()
+random_model = load_random_model()
 
 name = st.sidebar.text_input("Enter your name:")
 age = st.sidebar.slider("Enter your age", min_value = 0, max_value = 100, value = 24)
@@ -50,7 +50,7 @@ test_data = pd.DataFrame({
 if predict_btn:
     st.write(""" Test data: """)
     st.write(test_data)
-    pred = knn_model.predict(test_data)
+    pred = random_model.predict(test_data)
     st.write(""" Prediction result: """)
     st.write(pred)
     if pred is not None and len(pred)>0 and pred[0] == 1:
